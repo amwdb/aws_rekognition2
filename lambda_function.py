c14fb0612852534cabc14abf12d4c8e8641423d1
amwdb <simpleisbest916@gmail.com>
2023-11-18 20:09:37 +0900

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,14 @@
+def lambda_handler(event, context):
+
+    with open("index.html", 'r') as source_file: #画像含むバイナリの時はrb,テキストファイル場合はrのみ
+        source_code = source_file.read()
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': source_code,
+        "headers": {
+            'Content-Type': 'text/html'
+        }
+    }
+    
