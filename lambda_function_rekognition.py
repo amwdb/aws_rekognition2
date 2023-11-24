0b49133ceca4239ade364a79d005d976539c77b2
amwdb <simpleisbest916@gmail.com>
2023-11-24 21:04:06 +0900

    Update lambda_function_rekognition.py

--- lambda_function_rekognition.py
+++ lambda_function_rekognition.py
@@ -1,30 +1,52 @@
-#rekognition解析用
+import base64
 import boto3
+from cgi import parse_multipart, parse_header
+from io import BytesIO
 
 def lambda_handler(event, context):
-    print(event)
-    # -*- coding: utf-8 -*-
+    print(f"{event=}")
+    print(f"{context=}")
+
+    clt = boto3.client("rekognition")
+
+    # body部分にエンコードされた送信データが含まれている
+    body = event["body"]
+    #print(f"{body=}")
+    decoded = base64.b64decode(body)
+    #print(f"{decoded=}")
+    #改行b"\r\n"バイト型の改行
+    #elems = decoded.split(b"\r\n")
+    #for elem in elems:
+    #    print(elem)
+    
+    c_type, c_data = parse_header(event['headers']['content-type'])
+    assert c_type == 'multipart/form-data'
+    decoded_string = base64.b64decode(event['body'])
+    #For Python 3: these two lines of bugfixing are mandatory
+    #see also: https://stackoverflow.com/questions/31486618/cgi-parse-multipart-function-throws-typeerror-in-python-3
+    c_data['boundary'] = bytes(c_data['boundary'], "utf-8")
+    c_data['CONTENT-LENGTH'] = event['headers']['content-length']
+    form_data = parse_multipart(BytesIO(decoded_string), c_data)
+    #print(form_data.keys())
+    for image_str in form_data['image']:
+        print(image_str)
+    
+    
     #boto3のclient作成、rekognitionとリージョンを指定
     client = boto3.client('rekognition','ap-northeast-1')
-    picture_sample ='dog01.jpg'
-    # 画像ファイルを読み込んでバイト列を取得
-    with open(picture_sample, 'rb') as source_image:
-        source_bytes = source_image.read()
-
     # rekognitionのdetect_labelsにバイト列を渡してラベル検出実行
     response = client.detect_labels(
-       Image={
-           'Bytes': source_bytes
-       }
+        Image={
+            'Bytes': image_str
+        }
     )
-
     # 返ってきたresponseからラベル名(Name)と確度(Confidence)を整形して出力
-    for label in response['Labels']:
-        print("{Name:30},{Confidence:.2f}%".format(**label))
-    
-    
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    #for label in response['Labels']:
+        #print("{Name:30},{Confidence:.2f}%".format(**label))
+    results = []
+    for label in response["Labels"]:
+        s = "{Name:30},{Confidence:.2f}%".format(**label)
+        print(s)
+        results.append(s)
+    result = "\r\n".join(results)
+    return result
